2b5fa6702ac7542b525ad3e7aa131443b29af397
eric <553569682@qq.com>
2019-04-14 01:23:10 +0800

    ngx log monitor

--- nginxLog/NgxLog.py
+++ nginxLog/NgxLog.py
@@ -14,11 +14,20 @@ file = None
 183.206.18.237 - - [13/Apr/2019:12:49:16 -0400] "GET /tm.jpg HTTP/1.1" 304 0 "https://mail.qq.com/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36" "-"
 '''
 
+ops = {
+    'datetime': lambda timestr: datetime.strptime(timestr, "%Y-%m-%d %H:%M:%S %z"),
+    'status': int,
+    'size': int
+}
+
 
 def extract(line):
     regex = re.compile(pattern)
     matcher = regex.match(line)
-    return matcher.groupdict()
+    if matcher:
+        return {k: ops.get(k, lambda x: x)(v) for k, v in matcher.groupdict().items()}
+    else:
+        raise Exception('No match')
 
 
 class ProcessTransientFile(ProcessEvent):
